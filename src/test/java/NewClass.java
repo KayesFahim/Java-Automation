301dbf3c79fecd3ab541e28f304b114a3c5419d3
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-10-23 01:37:53 +0600

    Upto review page

--- src/test/java/NewClass.java
+++ src/test/java/NewClass.java
@@ -48,11 +48,94 @@ public class NewClass {
                         wait.until(ExpectedConditions.elementToBeClickable(By.id("mat-select-value-3"))).click();
                         driver.findElement(By.xpath("//*[@id=\"mat-option-3\"]")).click();
                         
-                        Thread.sleep(20000);
+                        Thread.sleep(5000);
                         
                         driver.findElement(By.xpath("/html/body/app-root/div/app-eligibility-criteria/section/form/mat-card[2]/button")).click();
 
+                        Thread.sleep(5000);
                         
+                        //First Name*
+                        driver.findElement(By.id("mat-input-2")).sendKeys("Kayes");
+                        //Last Name*
+                        driver.findElement(By.id("mat-input-3")).sendKeys("Fahim");
+                        //Gender*
+                        wait.until(ExpectedConditions.elementToBeClickable(By.id("mat-select-value-7"))).click();
+                        driver.findElement(By.id("mat-option-6")).click();
+                        
+                        //date of Birth
+                        driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]")).sendKeys("01111998");
+                        
+                        
+                        //nationality
+                        
+                        wait.until(ExpectedConditions.elementToBeClickable(By.id("mat-select-value-9"))).click();
+                        driver.findElement(By.id("mat-option-9")).click();
+                        
+                        
+                        
+                        //Passport Number
+                        
+                         driver.findElement(By.id("mat-input-4")).sendKeys("BN977044");
+                         
+                         // Passport Date picker
+                        driver.findElement(By.xpath("//*[@id=\"passportExpirtyDate\"]")).sendKeys("02012022");
+                         
+                         
+                         //number code
+                         
+                         driver.findElement(By.id("mat-input-5")).sendKeys("44");
+                         
+                         //Mobile Number
+                         driver.findElement(By.id("mat-input-6")).sendKeys("01685370455");
+                         
+                         //Email
+                         driver.findElement(By.id("mat-input-7")).sendKeys("kiddykayes@gmail.com");
+                         
+                         //Address 1
+                         driver.findElement(By.id("mat-input-8")).sendKeys("Road 1 Basundhara Dhaka");
+                         
+                         //Address 2
+                         driver.findElement(By.id("mat-input-9")).sendKeys("House 12 Flat 34");
+                         
+                         //State
+                         driver.findElement(By.id("mat-input-10")).sendKeys("Dhaka");
+                         
+                         //City
+                         driver.findElement(By.id("mat-input-11")).sendKeys("Dhaka");
+                         
+                         //Postcode
+                         driver.findElement(By.id("mat-input-12")).sendKeys("1220");
+                         
+                         
+                        Thread.sleep(5000);
+                        
+                        driver.findElement(By.
+                                xpath("/html/body/app-root/div/app-applicant-details/section/mat-card[2]/app-dynamic-form/div/div/app-dynamic-control/div/div/div[2]/button")).click();
+                                   
+                        Thread.sleep(5000);
+                        
+                        driver.findElement(By.
+                                xpath("/html/body/app-root/div/app-applicant-details/section/mat-card[2]/div/div[2]/button")).click();
+                        
+                        Thread.sleep(5000);
+                        
+                        driver.findElement(By.cssSelector("date-availiable")).click();
+                        
+                        Thread.sleep(5000);
+                        
+                        driver.findElement(By.
+                                xpath("/html/body/app-root/div/app-book-appointment/section/mat-card[2]/div/div[2]/button")).click();
+                        
+                        Thread.sleep(3000);
+                        
+                        driver.findElement(By.id("STRadio1")).click();
+                        
+                        Thread.sleep(5000);
+                        driver.findElement(By.xpath("/html/body/app-root/div/app-book-appointment/section/mat-card[2]/div/div[2]/button")).click();
+                        
+                        
+                        
+                         
                         
                         
                         
